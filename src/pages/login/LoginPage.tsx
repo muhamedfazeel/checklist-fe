c4bf78318fb5dba6f770a0eca0df63c8e8b699ed
Muhamed Fazeel <fazeel642@gmail.com>
2024-05-22 17:42:56 +0530

    Render loader while fetching response from backend

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -4,11 +4,15 @@ import { httpService } from '../../main/services/httpService';
 import { AUTH_KEY } from '../../main/constants/constants';
 import { ENDPOINTS } from '../../main/constants/endpoints';
 import './LoginPage.css';
+import { useState } from 'react';
+import Loader from '../../components/atoms/loader/Loader';
 
 const LoginPage = () => {
 	const { setUserDataFromToken } = useAuth();
+	const [isLoading, setIsLoading] = useState(false);
 
 	const login = async (credentials: string) => {
+		setIsLoading(true);
 		const { data } = await httpService.post(
 			ENDPOINTS.auth.login,
 			{},
@@ -21,6 +25,7 @@ const LoginPage = () => {
 		const token = data.data.accessToken;
 
 		setUserDataFromToken(token);
+		setIsLoading(false);
 	};
 	const isUserLoggedIn = localStorage.getItem(AUTH_KEY);
 	return (
@@ -28,21 +33,24 @@ const LoginPage = () => {
 			{!isUserLoggedIn ? (
 				<div className='login-page'>
 					<div className='login-bg'></div>
-					<div className='btn-container'>
-						<button className='login-btn'>
-							<GoogleLogin
-								onSuccess={(response) => {
-									if (response?.credential) {
-										login(response.credential);
-									}
-								}}
-								onError={() => {
-									console.log('Login Failed');
-								}}
-								width={'100%'}
-							/>
-						</button>
-					</div>
+					{isLoading && <Loader />}
+					{!isLoading && (
+						<div className='btn-container'>
+							<button className='login-btn'>
+								<GoogleLogin
+									onSuccess={(response) => {
+										if (response?.credential) {
+											login(response.credential);
+										}
+									}}
+									onError={() => {
+										console.log('Login Failed');
+									}}
+									width={'100%'}
+								/>
+							</button>
+						</div>
+					)}
 				</div>
 			) : (
 				<div>
